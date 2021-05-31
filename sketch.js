d122ac025387d963e9e1bcab9dcfc5be7edef528
Abhinav122530 <74701991+Abhinav122530@users.noreply.github.com>
2021-05-31 20:01:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,8 +11,7 @@ var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango
 var world,boy;
 
 //Declare launcherObject and launchForce variable here
-var launcherObject;
-var launchForce=100
+
 
 function preload(){
 	boy=loadImage("images/boy.png");
@@ -41,7 +40,7 @@ function setup() {
 	treeObj=new tree(1050,580);
 	groundObject=new ground(width/2,600,width,20);
   //create launcherObject here
-launcherObject=new launcher(stoneObj.body,{x:235,y:420});
+
 
 	Engine.run(engine);
 }
@@ -71,7 +70,7 @@ function draw() {
   stoneObj.display();
   groundObject.display();
   // display launcher object here
-    launcherObject.display();
+    
 
 
   detectollision(stoneObj,mango1);
@@ -89,28 +88,13 @@ function draw() {
 }
 
 //create mouseDragged function here
-function mouseDragged(){
-  Matter.Body.setPosition(stoneObj.body, {x: mouseX , y: mouseY});
 
-
-  
-}
 
 //create mouseReleased function here
-function mouseReleased(){
-launcherObject.fly();
 
-}
 
 //create keyPressed function here
-function keyPressed(){
-if (keyCode===32){
-Matter.Body.setPosition(stoneObj.body, {x:235,y:420})
-launcherObject.attach(stoneObj.body);
-}
 
-
-}
 
   function detectollision(lstone,lmango){
 
